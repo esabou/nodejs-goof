057cd3def4062b75d1fe5afd3e749562555be264
edd <154980296+esabou@users.noreply.github.com>
2025-08-29 15:47:07 +0300

    Update fetch_ids.sh

--- .github/workflows/fetch_ids.sh
+++ .github/workflows/fetch_ids.sh
@@ -10,7 +10,7 @@ fi
 API_BASE="https://api.snyk.io"
 
 # Fetch all groups
-groups_json=$(curl -s -X GET "$API_BASE/rest/groups" \
+groups_json=$(curl -s -X GET "$API_BASE/rest/groups?version=2024-10-15" \
   -H "Authorization: token $SNYK_ADMIN_TOKEN" \
   -H "Content-Type: application/json")
 
@@ -22,7 +22,7 @@ GROUP_ID=$(echo "$groups_json" | jq -r '.groups[0].id')
 echo "✅ Using GROUP_ID: $GROUP_ID"
 
 # Fetch all service accounts in that group
-accounts_json=$(curl -s -X GET "$API_BASE/rest/groups/$GROUP_ID/service_accounts" \
+accounts_json=$(curl -s -X GET "$API_BASE/rest/groups/$GROUP_ID/service_accounts?version=2024-10-15" \
   -H "Authorization: token $SNYK_ADMIN_TOKEN" \
   -H "Content-Type: application/json")
 
